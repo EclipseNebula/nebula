30e95bfc1470ec728537d029459c2fe5e7e20db4
Dirk Fauth <dirk.fauth@googlemail.com>
2026-02-13 13:20:22 +0100

    Fix build issues related to Java version and bundle version

--- widgets/richtext/org.eclipse.nebula.widgets.richtext.example.e4/src/org/eclipse/nebula/widgets/richtext/example/e4/part/JFaceViewerIntegrationExamplePart.java
+++ widgets/richtext/org.eclipse.nebula.widgets.richtext.example.e4/src/org/eclipse/nebula/widgets/richtext/example/e4/part/JFaceViewerIntegrationExamplePart.java
@@ -15,7 +15,7 @@
  *****************************************************************************/
 package org.eclipse.nebula.widgets.richtext.example.e4.part;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.eclipse.nebula.widgets.richtext.example.JFaceViewerIntegrationExample;
 import org.eclipse.swt.widgets.Composite;
